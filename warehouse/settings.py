4f6f278f80cf83abd4c61246804ee42ad0268676
tharaka <tharaka@gmail.com>
2024-03-21 06:45:11 +0530

    add postgress as the database

--- warehouse/settings.py
+++ warehouse/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'warehouse.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'warehouse', 
+        'USER': 'user',
+        'PASSWORD': 'localdev',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
